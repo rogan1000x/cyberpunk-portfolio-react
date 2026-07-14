13634533ec2a8ca2e2b6861979bb416708606980
Rogan Kim <rogan1000x@gmail.com>
2026-07-14 21:08:26 +0900

    day03: JSON 데이터 분리 - 플랫폼화 1단계 완료

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,7 @@
 import ProjectCard from './ProjectCard';
+import projects from './projects.json'; 
 
 function Home() {
-    const projects = [
-        {
-            title: "SEOUL-2077 HANGUL BREACH",
-            description: "한글 타이핑 게임",
-            link: "https://cyberfunk-hangle.netlify.app/"
-        },
-        {
-            title: "포트폴리오 사이트",
-            description: "사이버펑크 테마 포트폴리오",
-            link: "https://rogan-portfolio.netlify.app/"
-        }
-    ];
-
     return (
         <div>
             {projects.map((project, index) => (
